41418cacd973fe26ff20e3d12406c6834ed93412
Oliver García Montero <disrupted.night@gmail.com>
2019-01-21 07:54:29 -0600

    ListaSimple

---
+++ 02_ListaSimple/index.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: "app",
+    data: {
+        lista: [
+            {nombre: 'Maxwell'},
+            {nombre: 'thefallingboy'},
+            {nombre: 'Kreioz'}
+        ]
+    }
+})
